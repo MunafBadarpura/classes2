05cdd3b169d221f4386651380d9dcf70d5606589
Munaf Badarpura <munafbadarpura11@gmail.com>
2026-01-11 13:03:46 +0530

    build upadte 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        display: ["Fredoka", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          blue: "#4F46E5",
+          purple: "#9333EA",
+          light: "#F5F3FF",
+          accent: "#F472B6",
+        },
+      },
+      borderRadius: {
+        "4xl": "2.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
