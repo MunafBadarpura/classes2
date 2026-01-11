657b1ba6db62d5a770ea0352a20f202b63648f20
Munaf Badarpura <munafbadarpura11@gmail.com>
2026-01-11 13:00:58 +0530

    updated build errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-50%)" },
-        },
-      },
-      animation: {
-        marquee: "marquee 30s linear infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
